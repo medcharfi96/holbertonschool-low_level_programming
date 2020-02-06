e7cadb619610ae4f14b026cac3b85eeaaa561421
medcharfi96 <1389@holbertonschool.com>
2020-02-06 20:41:01 +0000

    10 pliiiz

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -7,17 +7,18 @@
 int main(void)
 {
 	int n,x;
+
 	for (n = 0 ; n < 100 ; n++)
 	{
 		if (n < 10 )
 		{
-for (x = 0 ; x<10 ;x++)
-{
-			putchar(n + '0');
-			putchar(x + '0');
-			putchar(',');
-			putchar(' ');
-		}
+			for (x = 0 ; x < 10 ;x++)
+			{
+				putchar(n + '0');
+				putchar(x + '0');
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
