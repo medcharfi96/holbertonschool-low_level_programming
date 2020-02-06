227906b231ed8024bccdd9f01e03a6243a16accb
medcharfi96 <1389@holbertonschool.com>
2020-02-06 14:54:12 +0000

    try 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
- * main show all number <10 
+ * main - show all number <10 
  * use only putchar
  * Return: if 0(mean true)
  **/
 int main(void)
 {
-int x;
-x = 0;
-while (x <= 9)
-{
-putchar(x + '0');
-x++;
-}
-putchar('\n');
-return (0);
-}
+	int x;
+
+	x = 0;
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar('\n');
+	return (0);
+
