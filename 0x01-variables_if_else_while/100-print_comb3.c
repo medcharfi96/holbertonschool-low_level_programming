b50fe9a5bc1bd71b970915289089dc6d3d7618ab
medcharfi96 <1389@holbertonschool.com>
2020-02-07 20:43:18 +0000

    100 try

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h> 
+/**
+* main - mainfonc  
+*auditeur medcharfi96
+* Return: if 0 (mean  true) 
+*/
+int main(void) {
+
+	int fl, sl; 
+	for (fl = 0; fl < 9; fl++) {
+	
+	for (sl = fln; sl < 10; sl++) {
+
+			putchar(fl + '0');
+			putchar(sl + '0');
+			if (fl < 9)
+			{
+				 putchar(',');
+				putchar(' ');
+			}
+		}
+	} 
+	putchar('\n'); 
+return (0);
+}
