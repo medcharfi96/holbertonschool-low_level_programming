448664ec0d67e7aa7758261c8e9ccd5b56830745
medcharfi96 <1389@holbertonschool.com>
2020-02-07 20:55:18 +0000

    100 try22555553

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 /**
-* main - mainfonc
-* auditeur medcharfi96
-* Return: if 0 (mean  true)
-*/
+ * main - mainfonc
+ * auditeur medcharfi96
+ * Return: if 0 (mean  true)
+ */
 int main(void) 
 {
 	int fl, sl;
 	for (fl = 0; fl < 9; fl++) 
-{
-	for (sl = fln+1; sl < 10; sl++) 
-{
+	{
+		for (sl = fl+1; sl < 10; sl++) 
+		{
 			putchar(fl + '0');
 			putchar(sl + '0');
 			if (fl < 8)
 			{
-				 putchar(',');
+				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
-return (0);
+	return (0);
 }
