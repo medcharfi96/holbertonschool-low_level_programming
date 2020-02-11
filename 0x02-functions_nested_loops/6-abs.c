6db40904900bfdce0d356c9d6cee0971ca0e11b6
medcharfi96 <1389@holbertonschool.com>
2020-02-11 13:09:41 +0000

    abs fct

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+*_abs - give the absulut value
+*@c : la variable locale
+* Return: if  0 (mean true )
+*/
+int _abs(int c)
+{
+if (c >= 0 )
+{
+return (c);
+else if (c < 0)
+{
+c=c*-1;
+return (c);
+}
+}
