784a81fe21b8c54b1801e7b803855531acb36283
medcharfi96 <1389@holbertonschool.com>
2020-02-11 18:34:45 +0000

    fanillayyyyyyy 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,54 +1,45 @@
 #include "holberton.h"
 /**
-*times_table - show all the multiplication
-* using only putchar
-* Return: Void
-*/
+ *times_table - show all the multiplication
+ * using only putchar
+ * Return: Void
+ */
 void times_table(void)
 {
-int x;
-int y;
-for (x = 0; x < 10; x++)
-{
-for (y = 0; y < 10; y++)
-{
-if ((x * y) < 10)
-{
-if (y != 9)
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(x * y + '0');
-_putchar(',');
-}
-else
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(x * y + '0');
-}
-}
-else if (x * y == 81)
-{
-_putchar(' ');
-_putchar((x * y) / 10 + '0');
-_putchar((x * y) % 10 + '0');
-}
-else if ((x * y) > 10)
-{
-if (y != 9)
-{
-_putchar(' ');
-_putchar((x * y) / 10 + '0');
-_putchar((x * y) % 10 + '0');
-_putchar(',');
-}
-else
-_putchar(' ');
-_putchar((x * y) / 10 + '0');
-_putchar((x * y) % 10 + '0');
-}
-_putchar('\n');
-}
-}
+	int x;
+	int y;
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if ((x * y) < 10)
+			{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(x * y + '0');
+				if (y != 9)
+				{
+				_putchar(',');
+				}			
+				
+			}
+			else if (x * y == 81)
+			{
+				_putchar(' ');
+				_putchar((x * y) / 10 + '0');
+				_putchar((x * y) % 10 + '0');
+			}
+			else 
+			{
+				_putchar(' ');
+				_putchar((x * y) / 10 +'0');
+				_putchar((x * y) % 10 +'0');
+				if (y != 9)
+				{
+				_putchar(',');
+				}
+			}
+		}
+	_putchar('\n');
+	}
 }
