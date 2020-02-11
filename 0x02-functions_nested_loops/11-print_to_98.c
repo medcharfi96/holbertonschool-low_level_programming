7d47fe8af9bafbdf1006e703fa63224319e6eef2
medcharfi96 <1389@holbertonschool.com>
2020-02-11 19:26:57 +0000

    try 1 fr 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,7 +10,7 @@ void print_to_98(int n)
 int x;
 if (n == 98)
 {
-printf("%d",n);
+printf("%d", n);
 }
 else if (n < 98)
 for (x = n; x <= 98; x++)
@@ -18,7 +18,7 @@ for (x = n; x <= 98; x++)
 if (x == 98)
 printf("%d", x);
 else
-printf("%d, ",x);
+printf("%d, ", x);
 }
 else
 {
@@ -27,7 +27,7 @@ for (x = n; x >= 98; x--)
 if (x == 98)
 printf("%d", x);
 else
-printf("%d, ",x);
+printf("%d, ", x);
 }
 }
 }
