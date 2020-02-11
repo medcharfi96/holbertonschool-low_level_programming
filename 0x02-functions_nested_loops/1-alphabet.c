d8541535686019bb7c68ac76335a1817f85c15e8
medcharfi96 <1389@holbertonschool.com>
2020-02-11 10:12:23 +0000

    try 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * main -show  all the caractere.  
+ * Return: 0 (mean true)
+ *
+ **/
+int main(void) 
+{
+	char x='a';
+	for (x='a' ; a<= 'z' ; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return 0;
+}
