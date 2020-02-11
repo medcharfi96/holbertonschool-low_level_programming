d1b65710f1f70a680fec4ec75b8e4214a42ff756
medcharfi96 <1389@holbertonschool.com>
2020-02-11 17:58:15 +0000

    oflqksfjzielfjzeli

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,32 +6,49 @@
 */
 void times_table(void)
 {
-int x; 
+int x;
 int y;
 for (x = 0; x < 10; x++)
 {
 for (y = 0; y < 10; y++)
 {
-if ((x * y) / 10 == 0)
+if ((x * y) < 10)
+{
+if (y != 9)
 {
 _putchar(' ');
 _putchar(' ');
-_putchar((x % 10) * (y % 10) + '0');
+_putchar(x * y + '0');
 _putchar(',');
-}
-else if (x * y == 81)
-{
-_putchar(' ');
-_putchar((x % 10) * (y % 10) + '0');
 }
 else
 {
 _putchar(' ');
 _putchar(' ');
-_putchar((x % 10) * (y % 10) + '0');
+_putchar(x * y + '0');
+}
+}
+else if (x * y == 81)
+{
+_putchar(' ');
+_putchar((x * y) / 10 + '0');
+_putchar((x * y) % 10 + '0');
+}
+else if ((x * y) > 10)
+{
+if (y != 9)
+{
+_putchar(' ');
+_putchar((x * y) / 10 + '0');
+_putchar((x * y) % 10 + '0');
 _putchar(',');
 }
+else
+_putchar(' ');
+_putchar((x * y) / 10 + '0');
+_putchar((x * y) % 10 + '0');
 }
 _putchar('\n');
+}
 }
 }
