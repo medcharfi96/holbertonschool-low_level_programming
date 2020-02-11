ac88d9ba5a2efbc5929fcd17e94bab8210577008
medcharfi96 <1389@holbertonschool.com>
2020-02-11 15:15:34 +0000

    8 up

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,7 +12,7 @@ for (x = 0; x < 24; x++)
 for (y = 0; y < 60; y++)
 {
 _putchar(x / 10 + '0');
-_putchar(x % 10 + '0'); 
+_putchar(x % 10 + '0');
 _putchar(':');
 _putchar(y / 10 + '0');
 _putchar(y % 10 + '0');
