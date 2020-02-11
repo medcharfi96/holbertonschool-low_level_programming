2ee7542772419ec17b040755359aaafe5a50c01f
medcharfi96 <1389@holbertonschool.com>
2020-02-11 12:08:19 +0000

    try 3 pliiiiz

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
-* _islower -show if low case or not 
+*_islower -show if low case or not
 * @c : variable pour le check
 * Return: 0 (mean true )
 **/
 int _islower(int c)
 {
-if ( c >= 'a' && c <= 'z')
+if (c >= 'a' && c <= 'z')
 {
 return (1);
 }
