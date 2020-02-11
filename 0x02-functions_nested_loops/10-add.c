af09f348b8933f1bed3f494c85278feb5ed70901
medcharfi96 <1389@holbertonschool.com>
2020-02-11 15:32:09 +0000

    10 try

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+*add - give the absulut value 
+*@x : la variable locale
+*@y :variable locale
+* Return: if  0 (mean true )
+*/
+int add(int x, int y)
+{
+return (x + y);
+}
