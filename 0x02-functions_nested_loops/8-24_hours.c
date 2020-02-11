f5f6f55da31adc5b67e6b89c5c34726620077e73
medcharfi96 <1389@holbertonschool.com>
2020-02-11 15:04:43 +0000

    try 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*jack_bauer - give all the time of the day
+*using only putchar
+* Return: nothing  (mean true )
+*/
+void jack_bauer(void)
+{
+int x, y; 
+for (x = 0; x < 24; x++)
+ {
+ for (y = 0; y < 60; y++)
+ {
+ _putchar(x / 10 + '0');
+ _putchar(x % 10 + '0'); 
+_putchar(':');
+_putchar(y % 10 + '0');
+_putchar('\n');
+ }
+ }
+}
