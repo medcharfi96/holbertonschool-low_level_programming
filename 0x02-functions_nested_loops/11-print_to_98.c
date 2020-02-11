cfdd5da8d2ccf8618427737494d907b63cb49060
medcharfi96 <1389@holbertonschool.com>
2020-02-11 18:42:47 +0000

    try 1 fr 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "holberton.h" 
+#include <stdio.h> 
+/** 
+* print_to_98 - fct of showing 
+* @n: variable locale
+* Return: nothing 
+*/
+void print_to_98(int n)
+{
+int i;
+if (n == 98)
+{
+printf("98\n");
+}
+else if (n < 98)
+{
+for (x = n; x < 98; x++)
+printf("%d, ", x);
+printf("98\n");
+}
