f41b1b8b4b025166fddef92977c61a1764d1f2c5
medcharfi96 <1389@holbertonschool.com>
2020-02-11 20:32:59 +0000

    emchiii

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,25 +15,24 @@ void times_table(void)
 		{
 			if ((x * y) < 10)
 			{
-			_putchar(' ');
-			_putchar(' ');
 			_putchar(x * y + '0');
 				if (y != 9)
 				{
 				_putchar(',');
 				}
+			_putchar(' ');
+			_putchar(' ');
 			}
 			else if (x * y == 81)
 			{
-				_putchar(' ');
 				_putchar((x * y) / 10 + '0');
 				_putchar((x * y) % 10 + '0');
 			}
 			else
 			{
-				_putchar(' ');
 				_putchar((x * y) / 10 + '0');
 				_putchar((x * y) % 10 + '0');
+				_putchar(' ');
 				if (y != 9)
 				{
 				_putchar(',');
