bfe9fc6a4d2dc37f92bb3f8e71966001b41195ae
medcharfi96 <1389@holbertonschool.com>
2020-02-11 15:12:28 +0000

    8 up

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,17 +6,17 @@
 */
 void jack_bauer(void)
 {
-int x, y; 
+int x, y;
 for (x = 0; x < 24; x++)
- {
- for (y = 0; y < 60; y++)
- {
- _putchar(x / 10 + '0');
- _putchar(x % 10 + '0'); 
+{
+for (y = 0; y < 60; y++)
+{
+_putchar(x / 10 + '0');
+_putchar(x % 10 + '0'); 
 _putchar(':');
 _putchar ( y / 10 + '0');
 _putchar(y % 10 + '0');
 _putchar('\n');
- }
- }
+}
+}
 }
