cd14a19254dacb17b816fc8f997a20a5858cd946
medcharfi96 <1389@holbertonschool.com>
2020-02-13 09:17:58 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n : variable de recurance
+ * Return: nothing.
+ */
+void print_line(int n)
+{
+int i;
+if (n != 0)
+{
+for (i = 0; i < n-1; i++)
+{
+_putchar(' ')
+}
+}
+_putchar('/n');
+}
