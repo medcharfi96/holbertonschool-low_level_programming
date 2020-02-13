146a4cd21f1bf31de602c0e5344527e174e56561
medcharfi96 <1389@holbertonschool.com>
2020-02-13 09:56:45 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * fizz_buzz - show all numbers from 1  to 100  without multiple of 3  and  5.
+ * 
+ * Return: nothing
+*/
+void fizz_buzz(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else if (i % 3 == 0  && i % 5 != 0)
+{
+printf("Fizz");
+printf(" ");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf("Buzz");
+printf(" ");
+}
+}
+else 
+printf("%d",i);
+printf(" ");
+}
+_putchar('\n');
+}
