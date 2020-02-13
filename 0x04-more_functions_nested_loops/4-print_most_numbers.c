696aae118df463d05872777c8d1eeb22eee184c7
medcharfi96 <1389@holbertonschool.com>
2020-02-13 08:37:18 +0000

    comit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - show all numbers.
+ *
+ * Return: 1 (mean true).
+ */
+void print_most_numbers(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+if (a == 0 || a == 4)
+{
+a++;
+}
+else
+{
+_putchar(a + '0');
+}
+}
+_putcahr('/n');
+}
