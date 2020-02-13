601903d7d065245a48d8ee46f11ad12989f237b6
medcharfi96 <1389@holbertonschool.com>
2020-02-13 09:08:47 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n : variable de recurance
+ * Return: nothing.
+ */
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; i++)
+{
+_putchar('_')
+}
+_putchar('/n');
+}
