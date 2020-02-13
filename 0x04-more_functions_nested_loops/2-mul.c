d78ae56fc4d0b5234e815fb7ad74166ea1440338
medcharfi96 <1389@holbertonschool.com>
2020-02-13 08:32:53 +0000

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * mul - multiplaying two int
+ * @a : int.
+ * @b : int.
+ * Return:  0 (mean true).
+ */
+int mul(int a, int b);
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - show all numbers.
+ *
+ * Return: 1 (mean true).
+ */
+void print_numbers(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+_putchar(a + '0');
+}
+_putcahr('/n');
+}
