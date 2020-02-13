d49fc2c6d9538f12efbc0206187077bef10388d0
medcharfi96 <1389@holbertonschool.com>
2020-02-13 18:51:32 +0000

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,18 +7,18 @@
  */
 void more_numbers(void)
 {
-int a;                            
+int a;
 int b;
-for (a = 0; a <= 10; a++)
+for (a = 0; a <= 9; a++)
 {
 for (b = 0; b <= 14; b++)
 {
 if (b / 10 != 0)
 {
 _putchar(b / 10 + '0');
-}                                      
+}
 _putchar(b % 10 + '0');
-}                                            
+}
 _putchar('\n');
 }
 }
