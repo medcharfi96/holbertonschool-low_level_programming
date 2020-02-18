138eec5d2d72146a86f2c06eae53efa5258e1b51
medcharfi96 <1389@holbertonschool.com>
2020-02-18 21:45:45 +0000

    adv

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _atoi - calculate the number in string
+ * @s: string
+ * Return: int (resultat avec le signe consideré).
+ */
+
+int _atoi(char *s)
+{
+	int a;
+	int res;
+	int type = 1;
+
+	a = 0;
+	res = 0;
+	while (((s + a) < '0' || (s + a) > '9') && ((s + a) != '\0'))
+	{
+		if (*(s + a) == '-')
+			type *= -1;
+		a++;
+	}
+	return (res);
+}
