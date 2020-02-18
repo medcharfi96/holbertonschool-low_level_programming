2c4bbc4e36368036dd552cb83d23a08dac0cd2d2
medcharfi96 <1389@holbertonschool.com>
2020-02-18 20:51:47 +0000

    tryyyyyyyyyyyyy

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,11 +10,13 @@ void print_rev(char *s)
 int x;
 int y;
 
-while (s[x] != '\n')
+while (s[x] != '\0')
 {
 x++;
 }
 for (y = x - 1; y >= 0; y--)
-_putchar(*(s[y]));
+{
+_putchar(s[y]);
 _putchar('\n');
+}
 }
