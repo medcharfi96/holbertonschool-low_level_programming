b653173b64593d388b9412d116231d98224842b1
medcharfi96 <1389@holbertonschool.com>
2020-02-20 21:34:07 +0000

    comm

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,17 +6,18 @@
  */
 char *leet(char *zab)
 {
-char a[] ="aAeEoOtTlL";
-char b[] ="4433007711";
-int i = 0;
-int j = 0;
-for(i = 0 ; zab[i] != '\0'; i++)
-{
-for( j =0 ; a[j] != '\0' ; j++)
-{
-if ( zab[i] == a[j])
-zab[i]=b[j];
-}
-} 
-return(zab);
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+	int i = 0;
+	int j = 0;
+
+	for (i = 0; zab[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (zab[i] == a[j])
+				zab[i] = b[j];
+		}
+	}
+	return (zab);
 }
