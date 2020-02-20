0dfa2e267237e25b4caceabd937f0becf5c8fdf9
medcharfi96 <1389@holbertonschool.com>
2020-02-20 15:05:14 +0000

    truuze

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * *string_toupper - string upccase
+ * @a: string
+ * Return: char (mean true).
+ */
+char *string_toupper(char *a)
+{
+int x ;
+while ( *(a +x) != '\0')
+{
+if (a[x]>= 'a' && a[x]<= 'z')
+{
+a[x]= a[x] - 32;
+}
+x++;
+}
+return (a);
+}
