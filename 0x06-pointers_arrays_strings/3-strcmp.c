07a8c5524ff04a9189cf2eba6197081ee8763676
medcharfi96 <1389@holbertonschool.com>
2020-02-20 20:03:14 +0000

    3aaa

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,24 +10,19 @@ int _strcmp(char *s1, char *s2)
 	int x;
 	int y;
 	int z;
+
 	x = 0;
 	y = 0;
 	z = 0;
-	while ( *(s1 +x) != '\0')
+	while ((*(s1 + x) != '\0') && ((*(s2 + y)) != '\0'))
 	{
-		x++;
-	}
-	while ( *(s2 + y) != '\0')
-	{
-		y++;
-	}
-	if ( x > y)
-	{
-		z = 1;
-	}
-	else if ( x < y)
-	{
-		z = -1;
+		if (*(s1 + x) == (*(s2 + y)))
+		{
+			x++;
+			y++;
+		}
+		else
+			z = s1[x] - s2[y];
 	}
 	return (z);
 }
