d89f1f0b4f5a0c18c2a5456498d85c89d68fbc85
medcharfi96 <1389@holbertonschool.com>
2020-02-20 19:41:01 +0000

    a tryy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,15 +11,15 @@ char *_strncat(char *dest, char *src, int n)
 int x;
 int y;
 
-x=0;
-y=0;
-while (*(dest+x) !=  '\0')
+x = 0;
+y = 0;
+while (*(dest + x) !=  '\0')
 {
 x++;
 }
-for (y = 0; y<= n-1; y++)
+for (y = 0; y <= n - 1; y++)
 {
-(*(dest + x + y ) )= *(src + y);
+(*(dest + x + y)) = *(src + y);
 }
 return (dest);
 }
