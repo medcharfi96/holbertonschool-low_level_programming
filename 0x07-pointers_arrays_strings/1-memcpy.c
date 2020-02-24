d38c924244a88d784d7383345212a34347908b01
medcharfi96 <1389@holbertonschool.com>
2020-02-24 16:20:41 +0000

    11111*

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,10 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-int i;
+unsigned int i;
 
 i = 0;
-for (i = 0; *(src + i) != '\0'; i++)
+for (i = 0; i < n; i++)
 {
 dest[i] = src[i];
 }
