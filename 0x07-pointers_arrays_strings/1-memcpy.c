113d782566cece43518a632cd960e24401de686a
medcharfi96 <1389@holbertonschool.com>
2020-02-24 09:13:43 +0000

    tryyy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *_memcpy - coppy from char to char
+ * @dest: char
+ * @src: char
+ * @n: int 
+ * Return: char.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+int i;
+
+i = 0;
+for (i = 0;*(src + i) != '\0';i++)
+{
+dest[i] = src[i];
+}
+return(*dest);
+}
