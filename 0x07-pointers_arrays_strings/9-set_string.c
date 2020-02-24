aa479b7c528040dd67475a6c8b8d23d71771f705
medcharfi96 <1389@holbertonschool.com>
2020-02-24 21:37:24 +0000

    9 en principe shiih

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * set_string - pointeur prend la valeur dun charactere
+ * @s: char
+ * @to: char
+ * Return: nothing(mean true).
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
