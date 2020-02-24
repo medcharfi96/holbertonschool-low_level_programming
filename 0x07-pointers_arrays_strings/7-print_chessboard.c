7960a1ff318ef9bb59703be38a61372607e808a5
medcharfi96 <1389@holbertonschool.com>
2020-02-24 21:04:49 +0000

    7777

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include"holberton.h"
+
+/**
+* print_chessboard - fonction affichage de chess
+* @a: char.
+*
+* Return: nothing(mean true)
+*/
+void print_chessboard(char (*a)[8])
+{
+int i;
+int j;
+
+i = 0;
+j = 0;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[j][i]);
+}
+}
