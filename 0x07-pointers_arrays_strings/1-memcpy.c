f849452a86b746d3c05c5c80e21c1f6de46e1f81
medcharfi96 <1389@holbertonschool.com>
2020-02-24 16:19:04 +0000

    11111*

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,7 @@
  * *_memcpy - coppy from char to char
  * @dest: char
  * @src: char
- * @n: int 
+ * @n: int
  * Return: char.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
@@ -12,9 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 int i;
 
 i = 0;
-for (i = 0;*(src + i) != '\0';i++)
+for (i = 0; *(src + i) != '\0'; i++)
 {
 dest[i] = src[i];
 }
-return(*dest);
+return (dest);
 }
