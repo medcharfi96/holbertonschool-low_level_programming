9e42af4fe69f25bdaa07855f91ee0bcecafc4446
medcharfi96 <1389@holbertonschool.com>
2020-02-24 10:45:42 +0000

    trrrrururururu

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * *_strstr - compare.
+ * @haystack: char
+ * @needle: char
+ * Return: char redendance.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+int j;
+
+i = 0;
+j = 0;
+for ( i = 0; *(haystack + i) != '\0'; i++)
+{
+while (*(needle) != '\0')
+{
+if (haystack[i] == needle[0])
+{
+j++;
+if (haystack[i + j] == needle[j])
+{
+j++
+}
+else 
+break;
+return (haystack[i]) 
+}
+} 
+}
+return(0);
+}
