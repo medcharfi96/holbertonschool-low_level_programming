f67d5881669c6ce8bc9bfaa38b7417c142c80bd9
medcharfi96 <1389@holbertonschool.com>
2020-02-24 20:46:46 +0000

    4444*

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -15,12 +15,13 @@ i = 0;
 j = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
-for (j =0; accept[j] != '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
 if (s[i] == accept[j])
 {
-return(*(s + i));
+return (&(s[i]));
 }
 }
 }
+return (0);
 }
