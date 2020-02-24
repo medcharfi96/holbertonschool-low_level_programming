44f2f9960193d465f3329fcc4d9abc9ab2d9da08
medcharfi96 <1389@holbertonschool.com>
2020-02-24 21:23:46 +0000

    com

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+* print_diagsums - calculate the diagonal result
+* @a: the pointer matrice
+* @size: matrice size
+* Return: nothing(mean true).
+*/
+void print_diagsums(int *a, int size)
+{
+int i;
+int diago1;
+int diago2;
+
+i = 0;
+diago1 = 0;
+diago2 = 0;
+for (i = 0; i < size; i++)
+{
+diago1 = diago1 + (a[(i * size) + i]);
+diago2 = diago2 + (a[(i * size) + (size - i)]);
+}
+printf("%d, %d", diago1, diago2);
+}
