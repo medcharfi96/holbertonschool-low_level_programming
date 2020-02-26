03ef9ed26657c9430d6f8f0fd5e43df221b2ca0e
medcharfi96 <1389@holbertonschool.com>
2020-02-26 22:23:03 +0000

    addddd

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,34 +1,15 @@
 #include "holberton.h"
 /**
- * is_palindrome - Tests a string if palindrome
- * @s: String
- * Return: int (mean true).
- */
-int is_palindrome(char *s)
-{
-int longeur;
-
-longeur = lengh(s);
-if (verif(s, 0, longeur) == 0)
-{
-return (0);
-}
-return (1);
-}
-/**
  * lengh - longeur de chaine
  * @chaine: char
  * Return: int(mean true).
  */
 int lengh(char *chaine)
 {
-int i;
-
 if (*chaine != '\0')
 return (0);
 else
-i = lengh(chaine + 1);
-return (1 + i);
+return (1 + lengh(chaine + 1));
 }
 /**
  * verif - algo de verification
@@ -43,5 +24,21 @@ if (chaine[x] != chaine[y])
 return (0);
 if (x >= y)
 return (1);
-return (verif(chaine, x + 1, y - 1));
+return (verif(x + 1, y - 1, chaine));
+}
+/**
+ * is_palindrome - Tests a string if palindrome
+ * @s: String
+ * Return: int (mean true).
+ */
+int is_palindrome(char *s)
+{
+int longeur;
+
+longeur = lengh(s);
+if (verif(0, longeur, s) == 0)
+{
+return (0);
+}
+return (1);
 }
