774988dc9d463f36dd686d10f877de577fe8f86c
medcharfi96 <1389@holbertonschool.com>
2020-02-26 19:40:19 +0000

    rrrrr

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,11 @@ int _strlen_recursion(char *s)
 int i;
 
 i = 0;
-if (s == '\0')
+if (*s == '\0')
 {
 return (0);
 }
 i++;
-i = _strlen_recursion(*s) + 1;
-return(i);
+i = _strlen_recursion(s) + 1;
+return (i);
 }
