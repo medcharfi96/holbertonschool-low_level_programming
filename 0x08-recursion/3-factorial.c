dde01b824064b32e0f8cf303036ecff9e33e87a4
medcharfi96 <1389@holbertonschool.com>
2020-02-26 18:14:55 +0000

    3 task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* factorial - calculer le factoriel dun entier.
+* @n : int
+* Return: int (mean true).
+*/
+int factorial(int n)
+{
+int res;
+
+res = 0;
+if (n >= 1)
+{
+res = (factorial(n) * n);
+return (res);
+}
+if (n == 0)
+{
+return (1);
+}
+else
+{
+return (1);
+}
+}
