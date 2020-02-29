0fe9fe31e82d3c74dbadded6cd071a6f4f996581
medcharfi96 <1389@holbertonschool.com>
2020-02-29 11:26:09 +0000

    tryyy

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - fonction to write the file name
+ * @argc: int.
+ * @argv: char containt the commande line
+ * Return: 0(mean true).
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+#holberton argc argv task
