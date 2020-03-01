3ccd3cf774c49ad4bdedfceedc8ba7643a1f35a6
medcharfi96 <1389@holbertonschool.com>
2020-03-01 20:43:36 +0000

    addd

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - addition des entiers.
+ * @argc: int.
+ * @argv : char.
+ * Return: 0(mean true).
+ */
+int main(int argc, char  *argv[])
+{
+int i = 1;
+int res;
+
+res = 0;
+for (i = 1; i < argc; i++)
+{
+if ((*argv[i] <= 48 && *argv[i] <= 57))
+{
+printf("Error\n");
+return (1);
+break;
+}
+else
+{
+res = res + atoi(*argv[i]);
+}
+i++;
+}
+printf("%d\n", res);
+return (0);
+}
