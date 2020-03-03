d686e7f117d4aafb45ed17b1010bde3dd66c7901
medcharfi96 <1389@holbertonschool.com>
2020-03-03 16:40:19 +0000

    commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+* create_array -crezte an array of char.
+* @size: int.
+* @c: char.
+* Return: array(mean true).
+*/
+char *str_concat(char *s1, char *s2)
+{
+int ls1, ls2, i, j;
+char *final;
+
+ls1 = 0;
+ls2 = 0;
+while (s1[ls1] != '\0')
+{
+ls1++;
+}
+while (s2[ls2] != '\0')
+{
+ls2++;
+}
+final = malloc(sizeof(char) * (ls1 + ls2) + 1);
+if(final == NULL)
+return(NULL);
+for(i = 0; i < ls1; i++)
+{
+final[i] = s1[i]
+}
+for(j = 0; j < ls2; j++)
+{
+final[i + j] = s2[j];
+}
+final[ls1 + ls2 + 1] = '\0';
+return (final);
+}
