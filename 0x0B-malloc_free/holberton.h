d1652349a79213b93a184122514ea30111d2c7fe
medcharfi96 <1389@holbertonschool.com>
2020-03-03 22:58:59 +0000

    tryyyy

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,3 +1,5 @@
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
 int **alloc_grid(int width, int height);
 char *str_concat(char *s1, char *s2);
 char *create_array(unsigned int size, char c);
