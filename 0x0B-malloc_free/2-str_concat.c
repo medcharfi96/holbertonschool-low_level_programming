3616c3146b8bb3345433808c3b747e330fc71902
medcharfi96 <1389@holbertonschool.com>
2020-03-03 16:56:21 +0000

    tryy

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
-* create_array -crezte an array of char.
-* @size: int.
-* @c: char.
-* Return: array(mean true).
+* *str_concat -concat two char.
+* @s1: char.
+* @s2: char.
+* Return: char(mean true).
 */
 char *str_concat(char *s1, char *s2)
 {
@@ -21,14 +21,14 @@ while (s2[ls2] != '\0')
 {
 ls2++;
 }
-final = malloc(sizeof(char) * (ls1 + ls2) + 1);
-if(final == NULL)
-return(NULL);
-for(i = 0; i < ls1; i++)
+final = malloc(sizeof(char) * (ls1 + ls2 + 1));
+if (final == NULL)
+return (NULL);
+for (i = 0; i < ls1; i++)
 {
-final[i] = s1[i]
+final[i] = s1[i];
 }
-for(j = 0; j < ls2; j++)
+for (j = 0; j < ls2; j++)
 {
 final[i + j] = s2[j];
 }
