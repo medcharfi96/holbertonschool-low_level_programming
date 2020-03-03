ed004ff0972151f1123ed9271cb9ad88b2fba522
medcharfi96 <1389@holbertonschool.com>
2020-03-03 22:57:14 +0000

    comm

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include<stdlib.h>
+/**
+ * free_grid - vider
+ * @grid : int
+ * @height : int
+ * Return: 0(mean true)
+*/
+void free_grid(int **grid, int height)
+{
+int count;
+ 
+if (grid != NULL && height != 0)
+{
+while(count < height)
+{
+free(grid[count]);
+free(grid);
+}
+}
+}
