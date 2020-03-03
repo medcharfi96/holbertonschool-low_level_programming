c7ff55c933ab59a02b5b385bda98863f7993b2f3
medcharfi96 <1389@holbertonschool.com>
2020-03-03 17:13:27 +0000

    3 try

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - matrice of 0
+ * @width: int.
+ * @height: int.
+ * Return: matrice(mean true).
+ */
+int **alloc_grid(int width, int height)
+{
+int w, h;
+int *tab;
+
+if (height > 0 && width > 0)
+{
+tab = malloc(height * sizeof(int));
+if (tab == NULL)
+{
+reeturn (NULL);
+}
+else
+{
+for (h = 0; h < height; h++)
+{
+tab = malloc(width * sizeof(int));
+for (w = 0; w < width; w++)
+{
+tab[w][h] = 0;
+}
+}
+}
+return (tab);
+}
+else
+return (NULL);
+}
