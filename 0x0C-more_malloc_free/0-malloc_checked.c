4bed69899fe5d496b41c6bad32d4976d10dd479f
medcharfi96 <1389@holbertonschool.com>
2020-03-05 14:37:39 +0000

    where 98

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,14 +1,18 @@
 #include "holberton.h"
+#include <stdlib.h>
 /**
- * malloc_cheked - check the space located.
+ * malloc_cheked - allocate .
  * @b : int
  * Return: 0(mean true).
  */
 void *malloc_checked(unsigned int b)
 {
-char *a;
-a = malloc(sizeof(b));
-if (a =='\0')
-printf("98");
+void *h;
+
+h = malloc(sizeof(b));
+if (h == '\0')
+{
+exit(98);
+}
 return (0);
 }
