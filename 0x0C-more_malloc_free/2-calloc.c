f8dea9430f67adad613b5456bcc4a7fa7dbb9a8c
medcharfi96 <1389@holbertonschool.com>
2020-03-05 16:19:48 +0000

    go out

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - fonction of allocation of somethingg
+ * @nmemb: int
+ * @size: int
+ * Return: Ntg(mean trueeeeeeeeeeeeeeeeeee).
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+void *p;
+
+if ((size || nmemb) == 0)
+return (NULL);
+p = malloc(sizeof(nmemb) * size);
+if (p == '\0')
+return (NULL);
+else 
+while (i < size)
+p[i] = nmemb;
+return (p);
+}
