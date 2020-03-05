a3e3e2a885c9bc9b469af83f284e0ff53134309f
medcharfi96 <1389@holbertonschool.com>
2020-03-05 21:38:52 +0000

    mmmzmezm

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,7 +21,7 @@ if (s2 != '\0')
 	while (s2[l2] != '\0')
 		l2++;
 }
-	if (n > l2)
+	if (n < l2)
 		l2 = n;
 	ch = malloc(sizeof(char) * (l2 + l1 + 1));
 	if (ch == '\0')
@@ -34,11 +34,11 @@ if (s2 != '\0')
 		i++;
 	}
 i = 0;
-	while (i < l2)
+	while (i < n)
 	{
 		ch[i + l1] = s2[i];
 		i++;
 	}
-	ch[l2 + l1 - 2] = '\0';
+	ch[l2 + l1 - 1] = '\0';
 	return (ch);
 }
