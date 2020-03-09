cdaf399a7e2956ca3264ab79c45a3a9720846c57
medcharfi96 <1389@holbertonschool.com>
2020-03-09 22:31:48 +0000

    init

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *init_dog - initialisation dog
+ *@d: dog
+ *@name: char
+ *@age: flaot
+ *@owner: char
+ *return: nothing(mean true)
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
