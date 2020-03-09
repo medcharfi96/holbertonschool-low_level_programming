6a5ab84cd1cf1fa68adbe7cbd845ef56f75c30f8
medcharfi96 <1389@holbertonschool.com>
2020-03-09 22:25:05 +0000

    commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+* struct dog - structure od dog
+* @name: char
+* @age: float
+* @owner: char
+* Description: dog details
+*/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
