c17cba2c5ddf1f25de3f467d21bc4b2c08de1790
medcharfi96 <1389@holbertonschool.com>
2020-03-14 17:08:47 +0000

    commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - sum the para
+ * @n: int
+ * Return: int(mean true)
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+va_list ap;
+int somme = 0;
+unsigned int i = 0;
+
+if (n == 0)
+return (0);
+va_start(ap, n);
+while (i <= n)
+{
+somme += va_arg(ap, unsigned int);
+i++;
+}
+va_end(ap);
+return (somme);
+}
