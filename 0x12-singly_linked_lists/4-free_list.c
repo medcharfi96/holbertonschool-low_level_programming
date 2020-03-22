f2dcc09452234963e83154f87a84f7d4e4c73f53
medcharfi96 <1389@holbertonschool.com>
2020-03-22 15:03:19 +0000

    commit

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,15 +1,19 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h> 
 /**
  * free_list - free
  * @head: list
  *
  * Return: nothing(mean true)
  */
+
 void free_list(list_t *head)
 {
 list_t *l = head;
 
-while (l != NULL)
+while ((l = head) != NULL)
 {
 head = head->next;
 free(l->str);
