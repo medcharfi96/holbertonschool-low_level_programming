f1994ebe7097e37766dd26e1182e76854f5196dd
medcharfi96 <1389@holbertonschool.com>
2020-03-22 15:24:06 +0000

    adv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Before_main - print before the main
+ *
+ * Return: nothing(mean true)
+ */
+
+void Before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("\nI bore my house upon my back!\n");
+}
