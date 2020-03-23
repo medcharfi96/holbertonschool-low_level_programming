a3ce140ce0e145e2e15def41989926c94fac33ff
medcharfi96 <1389@holbertonschool.com>
2020-03-23 14:17:57 +0000

    commit

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add at the end of list
+ * @head: struct
+ * @str: string
+ * Return: struct(mean true)
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *nouveau = malloc(sizeof(listint_t));
+listint_t *tmp = *head;
+
+if (nouveau == NULL)
+{
+return (NULL);
+}
+else
+{
+nouveau->next = NULL;
+nouveau->n = n;
+if (tmp == NULL)
+{
+*head = nouveau;
+}
+else
+{
+while (tmp->next != NULL)
+{
+tmp = tmp->next;
+}
+tmp->next = nouveau;
+}
+return (*head);
+}
+}
