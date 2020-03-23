85fd31b06807b995dd53336522b253f34966a94a
medcharfi96 <1389@holbertonschool.com>
2020-03-23 12:28:56 +0000

    commit

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - print the element of the list
+ * @h: list
+ * Return: size_t(mean true)
+*/
+
+size_t print_listint(const listint_t *h)
+{
+size_t count = 0;
+int x = 0;
+
+if (h == NULL)
+{
+return ('\0');
+}
+while (h)
+{
+x = h->n;
+printf("%d\n",x);
+h = h->next;
+count++;
+}
+return (count);
+}
