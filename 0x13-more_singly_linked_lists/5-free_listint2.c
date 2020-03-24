2596286a13bd0fa3ae8955d4a0b9654cbf18e3e2
medcharfi96 <1389@holbertonschool.com>
2020-03-24 11:43:59 +0000

    commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free list
+ * @head: tete de list
+ * Return: nothing(mean true)
+*/
+
+void free_listint2(listint_t **head)
+{
+listint_t *l = NULL;
+if (head != NULL)
+{
+while (head != NULL)
+{
+l = *head;
+*head = (*head)->next;
+free(l);
+}
+*head = NULL ;
+}
+else
+return ( NULL);
+}
