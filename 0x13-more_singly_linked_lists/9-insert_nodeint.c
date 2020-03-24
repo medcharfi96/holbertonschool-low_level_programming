8f60dada44f5ad97947a73db2d03bc923e263f89
medcharfi96 <1389@holbertonschool.com>
2020-03-24 19:38:44 +0000

    try

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  * insert_nodeint_at_index - insert inode in fix position
  * @head : listint_t
@@ -9,33 +10,36 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *itm = malloc(sizeof(listint_t));
-listint_t *tmp = *head;
-unsigned int i = 0, verif = 0;
-
-while (head != NULL)
+unsigned int i = 0;
+listint_t *new;
+listint_t *tmp;
+tmp = *head;
+if (tmp != NULL)
 {
-i++;
-if (i == idx)
-{
-verif = 1;
-break;
-}
-tmp = tmp->next;
-}
-if (verif == 0)
+if (head == NULL)
 return (NULL);
+if (idx == 0)
+{
+new->next = *head;
+*head = new;
+return (new);
+}
 else
 {
-if (itm == NULL)
+while(i - 1 < idx)
 {
+tmp = tmp->next;
+i++;
+}
+}
+new = malloc(sizeof(listint_t));
+if (new == NULL)
 return (NULL);
+new->n = n;
+new->next = tmp->next;
+tmp->next = new;
+return (new);
 }
-itm->n = n;
-for (i =0; i<= idx; i++)
-tmp= tmp->next;
-itm->next = tmp->next;
-tmp->next= itm;
-}
-return(tmp);
+else 
+return (NULL);
 }
