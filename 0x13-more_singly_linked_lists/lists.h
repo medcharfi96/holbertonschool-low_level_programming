c69d45f3eb9bfae71542fa69fe8412cd7a8b5cd2
medcharfi96 <1389@holbertonschool.com>
2020-03-24 20:50:57 +0000

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ struct listint_s *next;
 } listint_t;
 void free_listint(listint_t *head);
 size_t print_listint(const listint_t *h);
+listint_t *reverse_listint(listint_t **head);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
