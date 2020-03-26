ce9c949427795c1980edcfd26d8b4319fabe859d
medcharfi96 <1389@holbertonschool.com>
2020-03-26 19:07:40 +0000

    commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - clear the bit
+ * @index: int
+ * @n: int
+ * Return: int(mean true)
+ */
+
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned int x =(sizeof(n) * 7) + 5, test = 0;
+
+if (index < x)
+{
+test = 1 << index;
+*n = *n & test;
+return (1);
+}
+return (-1);
+} 
