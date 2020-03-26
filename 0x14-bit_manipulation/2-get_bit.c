f7c1c98b2acfe23720b949778665f815e96d40c4
medcharfi96 <1389@holbertonschool.com>
2020-03-26 18:46:35 +0000

    offfffff

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * get_bit - give the val of bit
+ * @index: int
+ * @index: int
+ * Return: int(mean true)
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int i, test;
+
+i = sizeof(n) * 8;
+if (index < i)
+{
+test = 1 & (n >> index);
+if (test <= 1)
+return (test);
+}
+return (-1);
+}
