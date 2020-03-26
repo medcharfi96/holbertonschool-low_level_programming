6a3d3368953067cccd375988f834cb4874b23bac
medcharfi96 <1389@holbertonschool.com>
2020-03-26 16:50:46 +0000

    commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * binary_to_uint - from bin ton int
+ * @b: char 
+ * Return: int(mean true)
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int res = 0, i = 0, count = 0;
+
+if (b == NULL)
+return (0);
+while (b[i] != '\0')
+{
+if (b[i] == '0' || b[i] == '1')
+i++;
+else
+return (0);
+}
+i--;
+while (b[count] != '\0')
+{
+if (b[count] == '1')
+{
+res = res + (1 << i);
+}
+i--;
+count++;
+}
+return (res);
+}
