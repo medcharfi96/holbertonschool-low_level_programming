d04e60b6d0b1b553f81d3a89b275762aed0646cc
medcharfi96 <1389@holbertonschool.com>
2020-03-29 15:31:36 +0000

    commit

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * array_range - new array
+ * @min: int
+ * @max: int
+ * Return: int(mean true)
+ */
+
+int *array_range(int min, int max)
+{
+int *tab, count;
+
+count = 0;
+if (max < min)
+return (NULL);
+tab = malloc((max - min + 1) * sizeof(int));
+if (tab == NULL)
+return (NULL);
+while (count < (max -min +1)
+{
+tab[count] = min;
+min++;
+count++;
+}
+return (tab);
+}
