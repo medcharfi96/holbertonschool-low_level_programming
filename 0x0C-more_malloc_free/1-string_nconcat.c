d0f8dd304150547575ef461d81ecd49384ba358c
medcharfi96 <1389@holbertonschool.com>
2020-03-29 15:12:26 +0000

    commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,36 +9,39 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int l1, l2 = 0, i = 0;
-	char *ch;
+unsigned int l1 =0, l2 = 0, i = 0;
+char *ch;
 if (s1 != '\0')
 {
-	while (s1[l1] != '\0')
-		l1++;
+while (s1[l1] != '\0')
+l1++;
 }
 if (s2 != '\0')
 {
-	while (s2[l2] != '\0')
-		l2++;
+while (s2[l2] != '\0')
+l2++;
 }
-	if (n < l2)
-		l2 = n;
-	ch = malloc(sizeof(char) * (l2 + l1 + 1));
-	if (ch == '\0')
-	{
-		return (NULL);
-	}
-	while (i < l1)
-	{
-		ch[i] = s1[i];
-		i++;
-	}
+if (n < l2)
+l2 = n;
+ch = malloc(sizeof(char) * (l2 + l1 + 1));
+if (ch == '\0')
+{
+return (NULL);
+}
+else
+{
+while (i < l1)
+{
+ch[i] = s1[i];
+i++;
+}
 i = 0;
-	while (i < n)
-	{
-		ch[i + l1] = s2[i];
-		i++;
-	}
-	ch[n + l1] = '\0';
-	return (ch);
+while (i < l2)
+{
+ch[i + l1] = s2[i];
+i++;
+}
+}
+ch[n + l1] = '\0';
+return (ch);
 }
