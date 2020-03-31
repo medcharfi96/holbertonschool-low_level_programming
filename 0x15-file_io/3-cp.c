22c6db9483c00827d229ec2825d1ee3667a96f72
medcharfi96 <1389@holbertonschool.com>
2020-03-31 17:42:13 +0000

    commi

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,107 @@
+#include "holberton.h"
+
+/**
+ * eror_test - test
+ * @erreur: int
+ * @argv: array
+ * Return: nothing(meantrue)
+ */
+
+void eror_test(int erreur, char *argv[])
+{
+if (erreur == 1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[erreur]);
+exit(98);
+}
+if (erreur == 2)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+
+/**
+ * copier - copier
+ * @lire: int
+ * @buffer: buff1
+ * @ft: int
+ * @argv: char
+ */
+
+void copier(int lire, char *buffer, int ft,  char *argv[])
+{
+int lire2, erreur, ecrire;
+while (lire != -1)
+{
+ecrire = write(ft, buffer, lire);
+if (ecrire != lire)
+{
+erreur = 2;
+eror_test(erreur, char *argv[]);
+}
+lire2 = read(lire, buffer, 1024);
+}
+if (lire == -1)
+{
+erreur == 1;
+eror_test(erreur, char *argv[]);
+}
+}
+/**
+ * close - close file
+ * @ff: file to
+ * @ft: file to
+ * Return: nothing
+ */
+void close (int ff, int ft)
+{
+if (close (ff) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[1]);
+exit(100);
+}
+if (close(ft) == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv[2]);
+exit(100);
+}
+}
+/**
+ * main - function main
+ * @argc: int
+ * @argv: char
+ * Return: int(mean true)
+ */
+
+int main(int argc, char *argv[])
+{
+int ff, ft, lire, rd;
+char buffer[1024];
+
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+if (argv[1] == NULL)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+ff = open(argv[1], O_RDONLY);
+ft = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+if (ff == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+exit(98);
+}
+if (ft == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+exit(99);
+}
+lire = read(ff, buffer, 1024);
+copier(lire , buffer, ft, *argv[]);
+close(ff, ft);
+return (0);
+}
