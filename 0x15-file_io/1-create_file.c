92356185444096adfaa4c2c75b7b4c85bd9012e5
medcharfi96 <1389@holbertonschool.com>
2020-03-31 15:03:55 +0000

    try

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * create_file - fonction to new file
+ * @filename: char
+ * @text_content: char
+ * Return: int(meantrue)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+int fichier, fichier2, count = 0;
+
+if (filename == NULL)
+return (-1);
+fichier = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+if (fichier == -1)
+return (-1);
+if(text_content != NULL)
+{
+while (text_content[count] != '\0')
+count++;
+fichier2 = write(fichier, text_content, count);
+if (fichier2 != -1)
+return (1);
+}
+return (-1);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -4,3 +4,4 @@
 #include <unistd.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t lette);
+int create_file(const char *filename, char *text_content);
