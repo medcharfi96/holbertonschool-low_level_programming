38b89980e4e8279569f3661be865067c3e67d20d
medcharfi96 <1389@holbertonschool.com>
2020-03-31 15:05:15 +0000

    try

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int fichier, fichier2, count = 0;
 
 if (filename == NULL)
 return (-1);
-fichier = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 600);
+fichier = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 if (fichier == -1)
 return (-1);
 if(text_content != NULL)
