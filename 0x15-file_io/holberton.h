bdcb616a7c97dc98209ffece355dddf9c6a67296
medcharfi96 <1389@holbertonschool.com>
2020-03-31 19:55:22 +0000

    commit

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,8 +1,13 @@
-#include <sys/types.h>
-#include <sys/stat.h>
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 ssize_t read_textfile(const char *filename, size_t lette);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+#endif
