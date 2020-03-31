59cb53d80804ac2b4a392fa03029e0e1b55ef3ec
medcharfi96 <1389@holbertonschool.com>
2020-03-31 16:20:56 +0000

    commi

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,3 +5,4 @@
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t lette);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
