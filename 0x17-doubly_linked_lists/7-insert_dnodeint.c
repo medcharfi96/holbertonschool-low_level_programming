a34554f1253eda70347973398c4111f4a8f82d08
medcharfi96 <1389@holbertonschool.com>
2020-05-03 22:16:47 +0200

    commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,21 @@
 #include "lists.h"
+/**
+ * dlistint_len - Print lislenghtt elemennt
+ * @h: dlistint_t
+ * Return: int(mean true)
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t nbr;
 
+nbr = 0;
+while (h != NULL)
+{
+h = h->next;
+nbr++;
+}
+return (nbr);
+}
 
 /**
  * insert_dnodeint_at_index - add ind in choosen pos
@@ -12,28 +28,34 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *nouveau, *tmp = *h;
 	unsigned int i = 0;
+	unsigned int len = dlistint_len(*h);
 
 	nouveau = malloc(sizeof(dlistint_t));
 	if (h ==  NULL || nouveau == NULL)
 	return (NULL);
 	nouveau->n = n;
+	nouveau->next = nouveau->prev = NULL;
 	if (idx == 0)
 	{
-		nouveau = add_dnodeint(h, n);
+	nouveau = add_dnodeint(h, n);
+	return (nouveau);
 	}
-
 	for (i = 0; i < idx - 1; i++)
 	{
 		tmp = tmp->next;
 	}
-	if (tmp != NULL)
+
+	if (tmp != NULL && i < len)
 	{
 	nouveau->next = tmp;
 	nouveau->prev = tmp->prev;
 	tmp->prev->next = nouveau;
 	tmp->prev = nouveau;
 	return (nouveau);
-
+	}
+if (len + 1 == idx)
+{
+nouveau = add_dnodeint_end(h, n);
 }
 return (NULL);
 }
