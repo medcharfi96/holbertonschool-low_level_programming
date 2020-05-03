5253ee9ad4393e8dfa53a8d2aa13c3fd69e5a1ba
medcharfi96 <1389@holbertonschool.com>
2020-05-03 17:46:00 +0200

    c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Print lislenghtt elemennt
+ * @h: dlistint_t
+ * Return: int(mean true)
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t nbr;
+
+nbr = 0;
+if (h == NULL)
+return (nbr);
+else
+{
+while (h != NULL)
+{
+h = h->next;
+nbr++;
+}
+}
+return (nbr);
+}
