29bb64d4fa1042d03311610bfbf6ed5b4c504483
medcharfi96 <1389@holbertonschool.com>
2020-08-31 13:54:11 +0200

    add

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,10 @@
 #ifndef _SEARCH_ALGO_H
 #define _SEARCH_ALGO_H
+
 #include <stdlib.h>
 #include <stdio.h>
+
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
 #endif
